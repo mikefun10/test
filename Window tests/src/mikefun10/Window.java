bdc3e6d276dbc8b78253b5b0467429aae0137eac
mikefun10 <m.george398@gmail.com>
2014-02-14 20:10:47 +1100

    I don't know what I'm doing here

---
+++ Window tests/src/mikefun10/Window.java
@@ -0,0 +1,16 @@
+package mikefun10;
+ 
+	import java.util.*;
+	
+public class Window {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//System.out.println("Hi I think I have done this code enough by now :P");
+
+	}
+
+}
